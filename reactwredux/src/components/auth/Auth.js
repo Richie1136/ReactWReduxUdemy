c27a3d840051807872cd595fb902b525d204d56e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-20 09:56:35 -0500

    Importing the authActions and the useDispatch hook inside of the auth.js file to dispatch the login action when the form is submitted

--- reactwredux/src/components/auth/Auth.js
+++ reactwredux/src/components/auth/Auth.js
@@ -1,10 +1,23 @@
 import './Auth.css';
 
+import { authActions } from '../../store/index';
+import { useDispatch } from 'react-redux'
+
+
+
+
 const Auth = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(authActions.login())
+
+  }
   return (
     <main className='auth'>
       <section>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='control'>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
